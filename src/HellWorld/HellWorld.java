da7bff78ba969850c6152ad550884694d113f889
MJE2024 <jonas.olsson63@gmail.com>
2025-05-27 13:09:34 +0200

    New

--- src/HellWorld/HellWorld.java
+++ src/HellWorld/HellWorld.java
@@ -5,7 +5,8 @@ public class HellWorld {
     public static void main(String[] args) {
 
         System.out.println("Hell World");
-        System.out.println("TestTest");
+        System.out.println("Hello Dog");
+
 
     }
 
